77d83f47c8954bc535721db568182bcde450b387
PhamTuanDuongs <githubtokenss@gmail.com>
2023-02-26 21:03:16 +0700

    edit loginController file,  login jsp

--- AssignmentPRJ/src/java/controller/authentication/loginController.java
+++ AssignmentPRJ/src/java/controller/authentication/loginController.java
@@ -4,11 +4,13 @@
  */
 package controller.authentication;
 
+import dal.userDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import model.User;
 
 /**
  *
@@ -18,12 +20,21 @@ public class loginController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        String raw_name = req.getParameter("user");
+        String raw_pass = req.getParameter("pass");
+        userDBContext user = new userDBContext();
+        User u = user.get(raw_name, raw_pass);
+        if (u != null) {
+            req.getSession().setAttribute("user", u);
+            resp.getWriter().println("Login successful!");
+        } else {
+            resp.getWriter().println("Login failed");
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("view/authentication/login.jsp");
+               req.getRequestDispatcher("view/authentication/login.jsp").forward(req, resp);
     }
 
 }
