57d155b67fd4ef66bdec16398546da7320c9da40
PhamTuanDuongs <githubtokenss@gmail.com>
2023-02-27 21:17:26 +0700

    build Lecture model

---
+++ AssignmentPRJ/src/java/model/Lecture.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author duong
+ */
+public class Lecture {
+    private String course;
+    private String room;
+    private String group;
+    private String status;
+    private int slotId;
+
+    public Lecture() {
+    }
+
+    public Lecture(String course, String room, String group, String status, int slotId) {
+        this.course = course;
+        this.room = room;
+        this.group = group;
+        this.status = status;
+        this.slotId = slotId;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+    
+    
+}
