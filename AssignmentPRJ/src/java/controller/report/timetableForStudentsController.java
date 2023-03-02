da4386eb2525973a73b0a1e0c10aebd93835d136
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-02 22:14:27 +0700

    Update timetableForStudentsController.java
    Using java.util.Calendar to generate days of week

--- AssignmentPRJ/src/java/controller/report/timetableForStudentsController.java
+++ AssignmentPRJ/src/java/controller/report/timetableForStudentsController.java
@@ -10,6 +10,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Locale;
 import java.sql.Date;
 import java.util.ArrayList;
 import model.Lecture;
@@ -21,20 +25,143 @@ import model.User;
  */
 public class timetableForStudentsController extends BaseRequiredAuthenticatedControllerForStudent {
 
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String dateInput = req.getParameter("date");
+        String year_raw = req.getParameter("year");
+        LocalDate currentdate = LocalDate.now();
+        int currentYear = currentdate.getYear();
+        int[] listYear = {2021, 2022, 2023, 2024};
+        req.setAttribute("listYear", listYear);
+        ArrayList<String> list = new ArrayList<>();
+        if (year_raw == null) {
+            // get current year
+            list = getAllDayWeek(currentYear);
+        } else {
+            currentYear = Integer.parseInt(year_raw);
+            list = getAllDayWeek(currentYear);
+        }
+        req.setAttribute("yearCurrent", currentYear);
+        req.setAttribute("list", list);
+        if (req.getParameter("date") == null || dateInput == "2023") {
+            int currentWeek = getCurrentWeek();
+            ArrayList<String> allDay = getEachDayByWeek(currentWeek);
+            req.setAttribute("current", currentWeek);
+            req.setAttribute("days", allDay);
+        } else {
+            ArrayList<String> allDay = getEachDayByWeek(Integer.parseInt(dateInput));
+            req.setAttribute("days", allDay);
+        }
+
+        String raw_week = req.getParameter("weekId");
+        if (raw_week != null) {
+            ArrayList<String> date = getEachDayByWeekIndb(Integer.parseInt(raw_week));
+            String dateFrom = date.get(0);
+            String dateTo = date.get(date.size() - 1);
+            LectureDBContext le = new LectureDBContext();
+            ArrayList<Lecture> l = le.timetable(1, Date.valueOf(dateFrom), Date.valueOf(dateTo));
+            if (l == null) {
+                 
+            } else {
+                  req.setAttribute("schedule", l);
+            }
+        } else {
+            int currentw = getCurrentWeek();
+            ArrayList<String> date = getEachDayByWeekIndb(13);
+            String dateFrom = date.get(0);
+            String dateTo = date.get(date.size() - 1);
+            LectureDBContext le = new LectureDBContext();
+            ArrayList<Lecture> l = le.timetable(1, Date.valueOf(dateFrom), Date.valueOf(dateTo));
+            if (l == null) {
+                 
+            } else {
+                  req.setAttribute("schedule", l);
+            }
+          
+        }
+
+        req.getRequestDispatcher("view/lecture/lecture.jsp").forward(req, resp);
+    }
+
+    private int getCurrentWeek() {
+        Calendar cal = Calendar.getInstance();
+        int currentWeek = cal.get(Calendar.WEEK_OF_YEAR);
+        return currentWeek;
+    }
+
+    private int getTotalWeeksInYear(int year) {
+        int totalWeeks = 0;
+        Calendar calendar = Calendar.getInstance();
+        for (int month = 0; month < 12; month++) {
+            int day = 1;
+            do {
+                calendar.set(year, month, day);
+                if (calendar.get(Calendar.DAY_OF_WEEK) == 5) {
+                    totalWeeks++;
+                }
+                day++;
+            } while (day <= calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        }
+        return totalWeeks;
+    }
+
+    private ArrayList<String> getAllDayWeek(int year) {
+        ArrayList<String> list = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        int totalWeek = getTotalWeeksInYear(2023);
+        for (int i = 1; i <= totalWeek; i++) {
+            Calendar cal = Calendar.getInstance(Locale.GERMANY);
+            cal.set(Calendar.YEAR, year);
+            cal.set(Calendar.WEEK_OF_YEAR, i);
+            cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+            String weekday = "";
+//        resp.getWriter().print(sdf.format(cal.getTime()));
+            weekday = weekday + sdf.format(cal.getTime()) + "/";
+            cal.add(Calendar.DATE, 6);
+            weekday += sdf.format(cal.getTime());
+            list.add(weekday);
+        }
+        return list;
+
+    }
+
+    private ArrayList<String> getEachDayByWeek(int weekNumber) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.WEEK_OF_YEAR, weekNumber);
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        ArrayList<String> list = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM");
+        for (int i = 0; i < 7; i++) {
+            String date;
+            date = sdf.format(cal.getTime());
+            list.add(date);
+            cal.add(Calendar.DATE, 1);
+        }
+        return list;
+    }
+
+    private ArrayList<String> getEachDayByWeekIndb(int weekNumber) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.WEEK_OF_YEAR, weekNumber);
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        ArrayList<String> list = new ArrayList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        for (int i = 0; i < 7; i++) {
+            String date;
+            date = sdf.format(cal.getTime());
+            list.add(date);
+            cal.add(Calendar.DATE, 1);
+        }
+        return list;
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
-        request.getRequestDispatcher("view/lecture/lecture.jsp").forward(request, response);
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
-        String raw_std = request.getParameter("std");
-        String raw_from = request.getParameter("dateFrom");
-        String raw_to = request.getParameter("dateTo");
-        LectureDBContext le = new LectureDBContext();
-        ArrayList<Lecture> l = le.timetable(Integer.parseInt(raw_std), Date.valueOf(raw_from), Date.valueOf(raw_to));
-        request.setAttribute("schedule", l);
-        request.getRequestDispatcher("view/lecture/lecture.jsp").forward(request, response);
+
     }
 
 }
