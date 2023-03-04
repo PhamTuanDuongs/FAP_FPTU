4787e4aaf9b056710a5256cc442d86357350ea06
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-04 20:35:31 +0700

    refactor: change name of folder

--- AssignmentPRJ/src/java/controller/reportStudent/timetableForStudentsController.java
+++ AssignmentPRJ/src/java/controller/reportStudent/timetableForStudentsController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controller.report;
+package controller.reportStudent;
 
 import controller.authentication.BaseRequiredAuthenticatedControllerForStudent;
 import dal.LectureDBContext;
