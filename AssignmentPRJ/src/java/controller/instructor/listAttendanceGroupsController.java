fa8a223d18832005146d6c71b748b14f49b16596
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-06 00:07:56 +0700

    Feat: ListAttendanceGroupsController

---
+++ AssignmentPRJ/src/java/controller/instructor/listAttendanceGroupsController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.instructor;
+
+import controller.authentication.BaseRequiredAuthenticatedControllerForInstructor;
+import dal.ListAttendanceGroupsDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import model.User;
+import model.listAttendaneGroups;
+
+/**
+ *
+ * @author duong
+ */
+public class listAttendanceGroupsController extends BaseRequiredAuthenticatedControllerForInstructor {
+     protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            User user = (User)req.getSession().getAttribute("user");
+            LocalDate currentdate = LocalDate.now();
+            ListAttendanceGroupsDBContext l = new ListAttendanceGroupsDBContext();
+            ArrayList<listAttendaneGroups> list = l.allGroup(Date.valueOf("2023-03-20"), user.getInstructorId());
+            req.setAttribute("list", list);
+            req.getRequestDispatcher("view/Instructor/listAttendanceGroup.jsp").forward(req, resp);
+     }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+}
