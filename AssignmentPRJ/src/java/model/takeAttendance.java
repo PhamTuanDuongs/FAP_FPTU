a2756ec0eda12b5f120eb9e2179fa7970ea5cf7b
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-06 00:10:00 +0700

    feat: model takeAttendance

---
+++ AssignmentPRJ/src/java/model/takeAttendance.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author duong
+ */
+public class takeAttendance {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private String rollnumber;
+    private String groupName;
+    private int groupId;
+
+    public takeAttendance() {
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRollnumber() {
+        return rollnumber;
+    }
+
+    public void setRollnumber(String rollnumber) {
+        this.rollnumber = rollnumber;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+    
+    
+    
+}
