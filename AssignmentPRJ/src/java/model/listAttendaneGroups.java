7c1bc41e22655852e24ed5cc59f4fdfb5d60200e
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-06 00:09:03 +0700

    feat: model listAttendanceGroups

---
+++ AssignmentPRJ/src/java/model/listAttendaneGroups.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author duong
+ */
+public class listAttendaneGroups {
+
+    private TimeSlot time;
+    private String courseName;
+    private String groupName;
+    private String roomName;
+    private int groupId;
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public listAttendaneGroups() {
+    }
+
+    public TimeSlot getTime() {
+        return time;
+    }
+
+    public void setTime(TimeSlot time) {
+        this.time = time;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+ 
+    
+
+}
