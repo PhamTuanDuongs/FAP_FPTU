0cf831581a7e79838a76617682d1fd8061ca10f2
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-09 00:39:38 +0700

    Refactor: rename file

--- AssignmentPRJ/src/java/dal/SessionDBContext.java
+++ AssignmentPRJ/src/java/dal/SessionDBContext.java
@@ -13,45 +13,51 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.Lecture;
+import model.Course;
+import model.Department;
+import model.Group;
+import model.Instructor;
+import model.Room;
+import model.Session;
+import model.TimeSlot;
 
 /**
  *
  * @author duong
  */
-public class LectureDBContext extends DBContext<Lecture> {
+public class SessionDBContext extends DBContext<Session> {
 
     @Override
-    public void insert(Lecture model) {
+    public void insert(Session model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void update(Lecture model) {
+    public void update(Session model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void delete(Lecture model) {
+    public void delete(Session model) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public Lecture get(int id) {
+    public Session get(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public ArrayList<Lecture> all() {
+    public ArrayList<Session> all() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    public ArrayList<Lecture> timetable(int std, Date from, Date to) {
-        ArrayList<Lecture> lecture = new ArrayList<>();
+    public ArrayList<Session> timetable(int std, Date from, Date to) {
+        ArrayList<Session> lecture = new ArrayList<>();
         PreparedStatement stm = null;
         ResultSet rs = null;
         try {
-            String sql = "select g.Name,c.Code,r.rname,a.Status,l.TimeSlotID,DATEPART(WEEKDAY,Date) as WeekDay from Lecture l inner join Attendance a\n"
+            String sql = "select *,DATEPART(WEEKDAY,Date) as WeekDay from Lecture l inner join Attendance a\n"
                     + "on l.LectureID  = a.LectureID inner join [Group] g \n"
                     + "on l.GroupID = g.GroupID join Course c on \n"
                     + "g.CourseID = c.CourseID join Room r\n"
@@ -64,33 +70,68 @@ public class LectureDBContext extends DBContext<Lecture> {
             stm.setDate(3, to);
             rs = stm.executeQuery();
             while (rs.next()) {
-                Lecture l = new Lecture();
-                l.setGroupName(rs.getString("Name"));
-                l.setCourse(rs.getString("Code"));
-                l.setRname(rs.getString("rname"));
-                l.setStatus(rs.getString("Status"));
-                l.setSlot(rs.getInt("TimeSlotID"));
-                l.setWeekDay(rs.getInt("WeekDay"));
+                Session l = new Session();
+                Session s = new Session();
+                s.setDate(rs.getDate("Date"));
+
+                TimeSlot t = new TimeSlot();
+                t.setSlotId(rs.getInt("TimeSlotID"));
+                t.setTimeFrom(rs.getTime("TimeFrom"));
+                t.setTimeTo(rs.getTime("TimeTo"));
+                s.setSlot(t);
+
+                Group g = new Group();
+                g.setGroupId(rs.getInt("GroupID"));
+                g.setGroupName(rs.getString("Gname"));
+
+                Instructor i = new Instructor();
+                i.setInstructorId(rs.getInt("InstructorID"));
+                i.setFirstName(rs.getString("Firstname"));
+                i.setLastName(rs.getString("Lastname"));
+                i.setEmail(rs.getString("Email"));
+                i.setDob(rs.getDate("DOB"));
+                i.setGender(rs.getBoolean("Gender"));
+                i.setAddress(rs.getString("Address"));
+                i.setTelephone(rs.getString("Telephone"));
+
+                Department d = new Department();
+                d.setDeptId(rs.getInt("Deptid"));
+                d.setDeptName(rs.getString("DeptName"));
+                d.setDeptName(rs.getString("DeptCode"));
+                i.setDepart(d);
+                s.setInstructor(i);
+                Course c = new Course();
+                c.setCode(rs.getString("Code"));
+                c.setName(rs.getString("Cname"));
+                c.setCourseId(rs.getInt("CourseID"));
+                g.setCourse(c);
+                s.setGroup(g);
+
+                Room r = new Room();
+                r.setRoomId(rs.getInt("RoomID"));
+                r.setRname(rs.getString("rname"));
+                s.setRoom(r);
+
                 lecture.add(l);
             }
         } catch (SQLException ex) {
-            Logger.getLogger(LectureDBContext.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             try {
                 rs.close();
             } catch (SQLException ex) {
-                Logger.getLogger(LectureDBContext.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
             }
 
             try {
                 stm.close();
             } catch (SQLException ex) {
-                Logger.getLogger(LectureDBContext.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
             }
             try {
                 connection.close();
             } catch (SQLException ex) {
-                Logger.getLogger(LectureDBContext.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(SessionDBContext.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return lecture;
@@ -107,8 +148,8 @@ public class LectureDBContext extends DBContext<Lecture> {
 //        } else {
 //
 //        }
-        LectureDBContext le = new LectureDBContext();
-        ArrayList<Lecture> l = le.timetable(1, Date.valueOf("2023-03-20"), Date.valueOf("2023-03-24"));
+        SessionDBContext le = new SessionDBContext();
+        ArrayList<Session> l = le.timetable(1, Date.valueOf("2023-03-20"), Date.valueOf("2023-03-24"));
         System.out.println(l.size());
 
     }
