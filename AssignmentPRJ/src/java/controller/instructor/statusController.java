406ce9d222067a4bd9b298d411fa69297e76fb88
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-14 22:54:32 +0700

    Feat: statusController

---
+++ AssignmentPRJ/src/java/controller/instructor/statusController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.instructor;
+
+import controller.authentication.BaseRequiredAuthenticatedControllerForInstructor;
+import dal.listStudentDBContext;
+import dal.takeAttendanceDBContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import model.Attendance;
+import model.Student;
+import model.User;
+
+/**
+ *
+ * @author duong
+ */
+public class statusController extends BaseRequiredAuthenticatedControllerForInstructor {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+        String raw_groupid = request.getParameter("groupid");
+        try {
+            int groupid = Integer.parseInt(raw_groupid);
+            listStudentDBContext l = new listStudentDBContext();
+             ArrayList<Student> student = l.studentByGroupID(groupid);
+            request.setAttribute("list", student);
+            
+            takeAttendanceDBContext status = new takeAttendanceDBContext();
+            ArrayList<Attendance> attend = status.statusStudents(groupid);
+            request.setAttribute("listStudent", attend);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+        
+        request.getRequestDispatcher("view/Instructor/status.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+
+    }
+
+}
