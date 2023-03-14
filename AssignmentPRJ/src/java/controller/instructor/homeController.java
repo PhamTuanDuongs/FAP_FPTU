58d8f5c453eec44b516cac0a9bfc2ca5107b922d
PhamTuanDuongs <githubtokenss@gmail.com>
2023-03-14 22:54:07 +0700

    feat: home

---
+++ AssignmentPRJ/src/java/controller/instructor/homeController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.instructor;
+
+import controller.authentication.BaseRequiredAuthenticatedControllerForInstructor;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.User;
+
+/**
+ *
+ * @author duong
+ */
+public class homeController extends BaseRequiredAuthenticatedControllerForInstructor{
+
+  @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+        request.setAttribute("profile", user.getDisplayname());
+        request.getRequestDispatcher("view/Instructor/home.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response, User user) throws ServletException, IOException {
+    }
+    
+}
---
+++ AssignmentPRJ/web/view/Instructor/home.jsp
@@ -0,0 +1,36 @@
+<%-- 
+    Document   : home
+    Created on : Mar 14, 2023, 10:51:00 AM
+    Author     : duong
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Home Page</title>
+        <style>
+            body{
+               background-color: #EEEEEE;
+            }
+            .content{
+                color: #3E54AC;
+                width: 60rem;
+                height: 42rem;
+                margin: auto;
+                margin-left: 22rem;
+                text-align: center;
+                margin-top: 1rem;
+                line-height: 3rem;
+            }
+        </style>
+    </head>
+    <body>
+        <%@ include file = "sideBar.jsp" %>
+        <div class="content">
+        <h2>Hello ${requestScope.profile}</h2>
+        <h1>LECTURER</h1>
+        </div>
+    </body>
+</html>
